3273f067e45ec84a667564998cb6a1c677e1ea87
Chamerli-sh <fares.setbels@gmail.com>
2022-03-04 16:30:48 +0100

    Command System V1

--- src/Main.py
+++ src/Main.py
@@ -5,9 +5,25 @@ print(tasko.name)
 tasko.set_check()
 print(tasko.is_checked())
 
+
+
 tasklist = []
 
 tasklist.append(tasko)
+def check_list():
+    for task in tasklist:
+        print("`" + task.name + "`", str(task.is_checked()))
 
-for task in tasklist:
-    print("`" + task.name + "`")
+def line_return():
+    inp = input()
+    check_command(inp)
+
+def check_command(inputs):
+    match inputs.split()[0]:
+        case 'l':
+            check_list()
+            line_return()
+        case 'q':
+            exit()
+
+line_return()
