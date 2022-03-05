43240f57eb696a9998d646339a7506623c57fbd6
Chamerli-sh <fares.setbels@gmail.com>
2022-03-05 18:12:48 +0100

    Finally Fixing the Switch Probleme

--- src/Main.py
+++ src/Main.py
@@ -29,19 +29,19 @@ def add_task(flags):
     tasklist.append(Task(name))
 
 def switch_task(flags):
-    chan = ''
-    if isinstance(flags, str) or isinstance(channel, str):
+    channel = ''
+    if isinstance(flags, str) and isinstance(channel, str):
         if flags == 's':
-            chan = input(Fore.GREEN + "Please Enter the Index of the task that ou would like to swtich state: " + Fore.WHITE)
-        else: print(Fore.RED + "Are you joking with me, this is not a number at all")
+            channel = int(input(Fore.GREEN + "Please Enter the Index of the task that ou would like to swtich state: " + Fore.WHITE))
+        # else: print(Fore.RED + "Are you joking with me, this is not a number at all", channel)
     
     elif isinstance(flags, int):
-        chan = int(flags)
+        channel = int(flags)
     else:
         print("Crazy Boy")  
 
     try:
-        channel = int(chan)
+        channel = int(flags)
         tasklist[channel].set_check(not tasklist[channel].is_checked())  
     except:
         print(Fore.RED + "An Error has benn detected, \n " + Fore.YELLOW + "1) Maybe You didn't type a number, \n 2) Maybe the Index you used don't exists (use the command 'l' to show the tasks and their indexs, \n 3) Or maybe it's just the bad code :)")
